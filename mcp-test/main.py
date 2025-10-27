3592f7cee8ab94b74b30a6494fe3c5531593025a
Diya James <diyajames@192.168.1.9>
2025-10-27 09:26:48 +0530

    Fix Host

--- mcp-test/main.py
+++ mcp-test/main.py
@@ -56,4 +56,4 @@ def greet_user(name: str, style: str = "friendly") -> str:
 
 
 if __name__ == "__main__":
-    uvicorn.run(mcp.streamable_http_app , host="localhost" , port=port)
+    uvicorn.run(mcp.streamable_http_app, host="0.0.0.0", port=port)
