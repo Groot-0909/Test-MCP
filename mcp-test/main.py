f65ad871d862650396f5003ad9fabd0d101b03e6
Diya James <diyajames@192.168.1.9>
2025-10-27 08:54:36 +0530

    first commit

---
+++ mcp-test/main.py
@@ -0,0 +1,59 @@
+"""
+FastMCP quickstart example.
+
+cd to the `examples/snippets/clients` directory and run:
+    uv run server fastmcp_quickstart stdio
+"""
+
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import os
+import uvicorn
+
+load_dotenv()
+port = int(os.environ.get("PORT" , 8000))
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+@mcp.tool()
+def greet(name:str = "World") -> str:
+    return f"Hello {name}"
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://test")
+def get_greeting_test() -> str:
+    """Get a personalized greeting"""
+    return f"Hello, test!"
+
+
+# Add a prompt
+@mcp.prompt()
+def greet_user(name: str, style: str = "friendly") -> str:
+    """Generate a greeting prompt"""
+    styles = {
+        "friendly": "Please write a warm, friendly greeting",
+        "formal": "Please write a formal, professional greeting",
+        "casual": "Please write a casual, relaxed greeting",
+    }
+
+    return f"{styles.get(style, styles['friendly'])} for someone named {name}."
+
+
+if __name__ == "__main__":
+    uvicorn.run(mcp.streamable_http_app , host="localhost" , port=port)
